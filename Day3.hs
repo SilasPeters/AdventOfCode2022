5d6c46202753acd3b48e21ec60134733401e32ea
Silas Peters <silaspeters03@gmail.com>
2022-12-04 23:32:28 +0100

    Day 3, Q2

--- Day3.hs
+++ Day3.hs
@@ -8,10 +8,17 @@ answer question = do
   let duplicatesPerRucksack = map findDuplicateInRucksack parsedInput
   case question of
     1 -> print $ sum $ map priority duplicatesPerRucksack
-    2 -> print $ "Not implemented yet"
+    2 -> print $ sum $ map priority $ findBadges parsedInput
 
 findDuplicateInRucksack :: String -> Char
 findDuplicateInRucksack contents = head $ uncurry intersect $ splitAt (length contents `div` 2) contents
+
+findBadges :: [String] -> [Char]
+findBadges []        = []
+findBadges rucksacks = findBadge (take 3 rucksacks) : findBadges (drop 3 rucksacks)
+  where
+    findBadge :: [String] -> Char
+    findBadge [a,b,c] = head $ intersect a $ intersect b c
 
 priority :: Char -> Int
 priority c = let asciiValue = ord c in
