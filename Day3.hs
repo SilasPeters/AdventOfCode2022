5d9fd67fdc109dab7578eb68006a08d78c18a1bc
Silas Peters <silaspeters03@gmail.com>
2022-12-04 23:17:58 +0100

    D3, Q1

---
+++ Day3.hs
@@ -0,0 +1,20 @@
+import Data.List
+import Data.Char
+
+answer :: Int -> IO ()
+answer question = do
+  inputRaw <- readFile "Day3Input.txt"
+  let parsedInput = lines inputRaw
+  let duplicatesPerRucksack = map findDuplicateInRuckpack parsedInput
+  case question of
+    1 -> print $ sum $ map priority duplicatesPerRucksack
+    2 -> print $ "Not implemented yet"
+
+findDuplicateInRuckpack :: String -> Char
+findDuplicateInRuckpack contents = head $ uncurry intersect $ splitAt (length contents `div` 2) contents
+
+priority :: Char -> Int
+priority c = let asciiValue = ord c in
+  if asciiValue < ord 'a'
+    then asciiValue - ord '@' + 26
+    else asciiValue - ord '`'
